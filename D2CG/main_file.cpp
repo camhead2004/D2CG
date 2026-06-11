89edd6fe8495d8afc0a8eb1edafb011fb8dd49af
camhead2004 <camhead2004rs@gmail.com>
2026-06-11 21:07:34 +0330

    Set main window procedure

--- D2CG/main_file.cpp
+++ D2CG/main_file.cpp
@@ -1,7 +1,69 @@
 # include <iostream>
+# include <windows.h>
+	
+const RECT* const get_screens_coordinate() {
+    HWND run_time_terminal_windows_handle{ GetConsoleWindow() };
+    HMONITOR screens_handle{ MonitorFromWindow(run_time_terminal_windows_handle , MONITOR_DEFAULTTONEAREST) };
+    MONITORINFO screens_info{ sizeof(MONITORINFO) };
+    GetMonitorInfo(screens_handle, &screens_info);
+    static RECT output_screens_rects_value{ screens_info.rcWork };
+
+    return &output_screens_rects_value;
+}
+
+
+// #1 Set main window procedure 
+
+LRESULT CALLBACK main_windows_proc(HWND main_windows_handle , UINT message_type , WPARAM word_parameter , LPARAM long_parameter) {
+
+	switch (message_type) {
+
+	case WM_CREATE: {
+        HWND run_time_terminals_windows_handle{ GetConsoleWindow() };
+        auto run_time_terminals_windows_rects_values_ptr { get_screens_coordinate() };
+        SetWindowPos(run_time_terminals_windows_handle, 0, run_time_terminals_windows_rects_values_ptr->right - 500, 0, 500, 400, 0);
+		std::cout << "WM_CREATE " << '\n';
+		break;
+	}
+
+	default: {
+		return DefWindowProc(main_windows_handle, message_type, word_parameter, long_parameter);
+	}
+
+	}
+
+	return 0;
+}
+
+// #1
+
+
+// #2 Execute main windows in run time 
+
+void execute_the_main_window(COLORREF background_color) {
+    WNDCLASS windows_class{ .lpfnWndProc{ main_windows_proc } , .hInstance{ GetModuleHandle(0) } , .hCursor{ LoadCursor(NULL, IDC_ARROW) } , .hbrBackground{ CreateSolidBrush(background_color) } , .lpszClassName{ TEXT("Main Window") } };
+    RegisterClass(&windows_class);
+    HWND main_windows_handle{ CreateWindowEx(0 ,  windows_class.lpszClassName , TEXT("Windows.exe") , WS_OVERLAPPEDWINDOW | WS_VISIBLE , 0 , 0 , 1000 , 800 , 0 , 0 , windows_class.hInstance , 0) };
+    MSG windows_message{};
+
+	// #3 Get windows current message in run time and dispatch to main windows procedure
+
+    while (true) {
+
+        while (GetMessage(&windows_message, main_windows_handle, 0, 0)) {
+            TranslateMessage(&windows_message);
+            DispatchMessage(&windows_message);
+        }
+    }
+
+	// #3
+}
+
+// #2
 
 
 int main() {
+	execute_the_main_window(RGB(0, 0, 0));
 
 	return 0;
 }
