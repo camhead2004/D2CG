e74a0bda556761cecbdd1c157e315eedfde11bf8
camhead2004 <camhead2004rs@gmail.com>
2026-06-14 17:19:40 +0330

    Add ID2D1Factory* and ID2D1HwndRenderTarget* for drawing custom direct 2d geometries in static class windows

--- D2CG/main_file.cpp
+++ D2CG/main_file.cpp
@@ -12,6 +12,8 @@
 # include "main_file.hpp"
 # include "direct_2d_objects.hpp"
 	
+# define WM_INIT_FACTORY 1024
+
 const RECT* const get_screens_coordinate() {
     HWND run_time_terminal_windows_handle{ GetConsoleWindow() };
     HMONITOR screens_handle{ MonitorFromWindow(run_time_terminal_windows_handle , MONITOR_DEFAULTTONEAREST) };
@@ -27,7 +29,52 @@ const RECT* const get_screens_coordinate() {
 
 class StaticWindowsCustomGeometries {
     inline static std::vector<std::vector<std::unique_ptr<Direct2DGeometriesBase>>> static_windows_custom_direct_2d_geometries{};
+    inline static ID2D1Factory* current_static_windows_factory_ptr { nullptr };
+    inline static ID2D1HwndRenderTarget* current_static_windows_target_ptr{ nullptr };
 
+    // #15 Update the windows render target rects values if the procedures windows handle change in WM_MOUSEMOVE , WM_PAINT messages in run time
+
+    static bool update_the_render_target(const HWND* const procudure_windows_handle_ptr) {
+        RECT render_target_rects_values{ 0 , 0 , 0 , 0 };
+
+        if (current_static_windows_factory_ptr && current_static_windows_target_ptr) {
+            HWND render_target_windows_handle{ current_static_windows_target_ptr->GetHwnd() };
+            GetWindowRect(render_target_windows_handle, &render_target_rects_values);
+        }
+        
+        RECT procedure_windows_rects_values{};
+        GetWindowRect(*procudure_windows_handle_ptr, &procedure_windows_rects_values);
+
+        return (render_target_rects_values.left != procedure_windows_rects_values.left || render_target_rects_values.top != procedure_windows_rects_values.top || render_target_rects_values.right != procedure_windows_rects_values.right ||
+            render_target_rects_values.bottom != procedure_windows_rects_values.bottom);
+    }
+
+    // #15 
+
+
+    // #14 Update the factory and hwnd render target to draw direct 2d custom geometries based on this variables 
+
+    static HRESULT init_direct_factory_and_render_target_windows(const HWND* const static_windows_handle_ptr , ID2D1Factory** factory_ptr_ptr , ID2D1HwndRenderTarget** current_windows_renders_target_ptr_ptr) {
+        HRESULT output_result {};
+        
+        if (SUCCEEDED(D2D1CreateFactory(D2D1_FACTORY_TYPE_MULTI_THREADED, factory_ptr_ptr))) {
+            RECT static_windows_clients_rects_values{};
+
+            if (!GetClientRect(*static_windows_handle_ptr , &static_windows_clients_rects_values)) {
+                std::cout << "Error at getting the current rects values " << '\n';
+                return 0;
+            }
+
+            D2D1_HWND_RENDER_TARGET_PROPERTIES static_windows_properties{ .hwnd{*static_windows_handle_ptr} , .pixelSize{ D2D1::SizeU(static_windows_clients_rects_values.right , static_windows_clients_rects_values.bottom) } ,
+            .presentOptions{D2D1_PRESENT_OPTIONS_IMMEDIATELY} };
+            output_result = (*factory_ptr_ptr)->CreateHwndRenderTarget(D2D1::RenderTargetProperties() , static_windows_properties ,current_windows_renders_target_ptr_ptr);
+        }
+
+
+        return SUCCEEDED(output_result);
+    }
+
+    // #14 
 
 public :
 
@@ -41,6 +88,7 @@ public :
             WNDPROC old_windows_proc{ (WNDPROC)SetWindowLongPtr(*input_static_windows_handle_ptr, GWLP_WNDPROC, (LONG_PTR)static_windows_proc) };
             SetWindowLongPtr(*input_static_windows_handle_ptr, GWLP_USERDATA, (LONG_PTR)old_windows_proc);
             SetWindowSubclass(*input_static_windows_handle_ptr, static_windows_proc, current_windows_subclass_id, 0);
+            SendMessage(*input_static_windows_handle_ptr, WM_INIT_FACTORY, 0, 0);
             ++current_windows_subclass_id;
         }
     }
@@ -51,19 +99,16 @@ public :
 
     template<bool is_moving_geometry , GeometriesShape shape , typename FillsBrushType , typename StrokesBrushType>
     typename std::enable_if<!is_moving_geometry, void>::type add_custom_direct_2d_geometry(typename ShapeToDimensionType<shape>::Dimension dimension_values, GeometriesCustomStyle<FillsBrushType, StrokesBrushType> style) {
-
+     //   std::cout << "add_custom_direct_2d_geometry --> false " << '\n';
     }
 
     template<bool is_moving_geometry , GeometriesShape shape, typename FillsBrushType, typename StrokesBrushType , GeometriesMovingCtrl ctrl_type , GeometriesMovingSpace space_type>
     typename std::enable_if<is_moving_geometry, void>::type add_custom_direct_2d_geometry(typename ShapeToDimensionType<shape>::Dimension dimension_values, GeometriesCustomStyle<FillsBrushType, StrokesBrushType> style , 
         MovingGeometriesProp<space_type> geometries_ctrl_moving_properties) {
-
+        std::cout << "add_custom_direct_2d_geometry --> true " << '\n';
     }
 
     // #12
-
-
-
 
 private : 
     
@@ -78,11 +123,31 @@ private :
             PAINTSTRUCT windows_paint_info{};
             HDC static_windows_device_context_handle{ BeginPaint(current_static_windows_handle , &windows_paint_info) };
 
+            if (update_the_render_target(&current_static_windows_handle)) {
+
+                if (!SUCCEEDED(init_direct_factory_and_render_target_windows(&current_static_windows_handle, &current_static_windows_factory_ptr, &current_static_windows_target_ptr))) {
+                    std::cout << "Error at setting the windows render target " << '\n';
+                }
+            }
+
             EndPaint(current_static_windows_handle, &windows_paint_info);
             break;
         }
 
         case WM_MOUSEMOVE: {
+
+            if (update_the_render_target(&current_static_windows_handle)) {
+
+                if (!SUCCEEDED(init_direct_factory_and_render_target_windows(&current_static_windows_handle, &current_static_windows_factory_ptr, &current_static_windows_target_ptr))) {
+                    std::cout << "Error at setting the windows render target " << '\n';
+                }
+            }
+
+            break;
+        }
+
+        case WM_MOUSELEAVE: {
+            std::cout << "WM_MOUSELEAVE " << '\n';
             break;
         }
 
@@ -119,22 +184,74 @@ LRESULT CALLBACK main_windows_proc(HWND main_windows_handle , UINT message_type 
     StaticWindowsCustomGeometries a{};
     StaticWindowsCustomGeometries b{};
 
+    static ID2D1Factory* main_windows_factory_ptr{ nullptr };
+    static ID2D1HwndRenderTarget* main_windows_render_target_ptr{ nullptr };
+
+
 	switch (message_type) {
 
 	case WM_CREATE: {
         HWND run_time_terminals_windows_handle{ GetConsoleWindow() };
         auto run_time_terminals_windows_rects_values_ptr { get_screens_coordinate() };
         SetWindowPos(run_time_terminals_windows_handle, 0, run_time_terminals_windows_rects_values_ptr->right - 500, 0, 500, 400, 0);
-		
         
-        static_1 = CreateWindowEx(0, WC_STATIC, 0, WS_VISIBLE | WS_CHILD | WS_BORDER | SS_NOTIFY | SS_OWNERDRAW, 10, 10, 900, 380, main_windows_handle, 0, ((LPCREATESTRUCT)long_parameter)->hInstance, 0);
-        static_2 = CreateWindowEx(0, WC_STATIC, 0, WS_VISIBLE | WS_CHILD | WS_BORDER | SS_NOTIFY | SS_OWNERDRAW, 10, 400, 900, 340, main_windows_handle, 0, ((LPCREATESTRUCT)long_parameter)->hInstance, 0);
+        static_1 = CreateWindowEx(0, WC_STATIC, 0, WS_VISIBLE | WS_CHILD | WS_BORDER | SS_NOTIFY | SS_OWNERDRAW, 10, 10, 900, 380, main_windows_handle, (HMENU)10, ((LPCREATESTRUCT)long_parameter)->hInstance, 0);
+        static_2 = CreateWindowEx(0, WC_STATIC, 0, WS_VISIBLE | WS_CHILD | WS_BORDER | SS_NOTIFY | SS_OWNERDRAW, 10, 400, 900, 340, main_windows_handle, (HMENU)20 , ((LPCREATESTRUCT)long_parameter)->hInstance, 0);
         a(&static_1);
         b(&static_2);
 
+        //a.add_custom_direct_2d_geometry<false, GeometriesShape::ELLIPSE, ID2D1SolidColorBrush*, ID2D1SolidColorBrush*>(D2D1::Ellipse(D2D1::Point2F(30.0f, 30.0f), 50.0f, 50.0f), {});
 
+
+        /*
+        D2D1CreateFactory(D2D1_FACTORY_TYPE_SINGLE_THREADED, &main_windows_factory_ptr);
+        RECT main_windows_clients_rects{};
+        GetClientRect(main_windows_handle, &main_windows_clients_rects);
+        D2D1_HWND_RENDER_TARGET_PROPERTIES hwnd_prop{ .hwnd{main_windows_handle} , .pixelSize{ D2D1::SizeU(main_windows_clients_rects.right , main_windows_clients_rects.bottom) } , .presentOptions{D2D1_PRESENT_OPTIONS_IMMEDIATELY} };
+        HRESULT result_value{ main_windows_factory_ptr->CreateHwndRenderTarget(D2D1::RenderTargetProperties(), hwnd_prop, &main_windows_render_target_ptr) };
+        */
+        
         break;
 	}
+
+        /*
+    case WM_PAINT: {
+        PAINTSTRUCT paint_info{};
+        HDC device_context_handle{ BeginPaint(main_windows_handle , &paint_info) };
+
+        
+        main_windows_render_target_ptr->BeginDraw();
+        ID2D1SolidColorBrush* solid_brush_ptr{ nullptr };
+        ID2D1LinearGradientBrush* linear_gradient_brush_ptr{ nullptr };
+        ID2D1RadialGradientBrush* radial_gradient_brush_ptr{ nullptr };
+        D2D1_LINEAR_GRADIENT_BRUSH_PROPERTIES linear_prop{ .startPoint{D2D1::Point2F(240.0f , 240.0f)} , .endPoint{D2D1::Point2F(300.0f , 300.0f)} };
+        D2D1_RADIAL_GRADIENT_BRUSH_PROPERTIES radial_prop{ .center{D2D1::Point2F(300.0f , 300.0f)} , .radiusX{ 40.0f } , .radiusY{ 40.0f } };
+
+        ID2D1GradientStopCollection* stop_collection_ptr { nullptr };
+        D2D1_GRADIENT_STOP stops[4]{ {0.1f , D2D1::ColorF(D2D1::ColorF::Aqua) } , { 0.3f , D2D1::ColorF(D2D1::ColorF::BlueViolet) } , { 0.5f , D2D1::ColorF(D2D1::ColorF::Coral) }, { 0.8f , D2D1::ColorF(D2D1::ColorF::HotPink) } };
+        main_windows_render_target_ptr->CreateGradientStopCollection(stops, 4, D2D1_GAMMA_2_2, D2D1_EXTEND_MODE_WRAP, &stop_collection_ptr);
+
+        main_windows_render_target_ptr->CreateLinearGradientBrush(linear_prop , stop_collection_ptr , &linear_gradient_brush_ptr);
+        main_windows_render_target_ptr->CreateRadialGradientBrush(radial_prop , stop_collection_ptr , &radial_gradient_brush_ptr);
+
+
+
+        main_windows_render_target_ptr->CreateSolidColorBrush(D2D1::ColorF(D2D1::ColorF::BlueViolet) , &solid_brush_ptr);
+        //main_windows_render_target_ptr->CreateLinearGradientBrush( , );
+
+
+        main_windows_render_target_ptr->DrawEllipse(D2D1::Ellipse(D2D1::Point2F(300.0f, 300.0f), 100.0f, 100.0f) , radial_gradient_brush_ptr , 10.0f);
+        main_windows_render_target_ptr->FillEllipse(D2D1::Ellipse(D2D1::Point2F(300.0f, 300.0f), 60.0f, 60.0f) , radial_gradient_brush_ptr);
+
+
+
+        main_windows_render_target_ptr->EndDraw();
+
+
+        EndPaint(main_windows_handle, &paint_info);
+        return 0;
+    }
+        */
 
 
 	default: {
@@ -175,7 +292,6 @@ void execute_the_main_window(COLORREF background_color) {
 
 int main() {
     execute_the_main_window(RGB(0 , 0 , 0));
-
 
     return 0;
 }
